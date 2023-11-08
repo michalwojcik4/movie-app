287104e418a9e72dba28e492a35f393fff566f94
michalwojcik4 <mwjcik4@gmail.com>
2023-11-08 18:22:04 +0100

    css to navi

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,27 @@
 import { NavLink } from 'react-router-dom';
 
+import css from './Nav.module.css';
+
 export function Nav() {
   return (
-    <nav>
-      <ul>
+    <nav className={css.nav}>
+      <p className={css.nav__logo}>Movie <span>App</span></p>
+      <ul className={css.nav__list}>
         <li>
-          <NavLink to="/">Home</NavLink>
+          <NavLink className={css.nav__link} style={(props) => {
+                return {
+                  color: props.isActive ? "#FFF" : "#000",
+                  backgroundColor: props.isActive ? 'rgb(72, 103, 195)' : '#FFF'
+                };
+              }} to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink to="/movies">Movies</NavLink>
+          <NavLink className={css.nav__link} style={(props) => {
+                return {
+                  color: props.isActive ? "#FFF" : "#000",
+                  backgroundColor: props.isActive ? 'rgb(72, 103, 195)' : '#FFF'
+                };
+              }} to="/movies">Movies</NavLink>
         </li>
       </ul>
     </nav>
